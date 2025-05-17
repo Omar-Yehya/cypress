67ee29949a1f6b3696ac9c22dbba1fa000375431
Omar-Yehya <143041754+Omar-Yehya@users.noreply.github.com>
2025-05-17 16:56:22 -0500

    Project 3

--- cypress/e2e/ProjectFolder/03-bookingValidation.cy.js
+++ cypress/e2e/ProjectFolder/03-bookingValidation.cy.js
@@ -1,6 +1,8 @@
 ///<reference types="cypress"/>
 
+import { BookingValidation } from "../../pages/BookingValidation"
 
+const bookingValidation =  new BookingValidation()
 describe('Validate frontend booking web page', () =>{
     beforeEach(() =>{
         cy.visit('https://www.techglobal-training.com/frontend/booking')
@@ -8,34 +10,226 @@ describe('Validate frontend booking web page', () =>{
     })
 
     it('validate default book your trip form', () =>{
-        cy.get('[class$="ml-0"] .mr-1')
-        .should('be.enabled')
+        bookingValidation.getOneWayRadioButton()
+        .should('be.visible')
+        .and('be.enabled')
         .and('be.checked')
 
-        cy.get('label[class$="radio"] .mr-1')
-        .should('be.enabled')
+        bookingValidation.getRoundTripRadioButton()
+        .should('be.visible')
+        .and('be.enabled')
         .and('not.be.checked')
 
-        cy.contains('label', 'Cabin Class')
+        bookingValidation.getCabinClassLabel()
         .should('be.visible')
 
-        cy.contains('label', 'Cabin Class')
-        .nextAll('div.select')
-        .first()
-        .find('select')
+        bookingValidation.getCabinClassDropdown()
         .should('be.visible')
 
-        cy.contains('label', 'From')
-            .nextAll('div.select')
-            .first()
-            .find('select')
-            .should('be.visible')
-
+        bookingValidation.getFromLabel()
+        .should('be.visible')
         
+        bookingValidation.getFromDropdown()
+        .should('be.visible')
+
+        bookingValidation.getToLabel()
+        .should('be.visible')
+
+        bookingValidation.getToDropDown()
+        .should('be.visible')
+
+        bookingValidation.getDepartLabel()
+        .should('be.visible')
+
+        bookingValidation.getDepartDropDown()
+        .should('be.visible')
+
+        bookingValidation.getReturnLabel()
+        .should('be.visible')
+
+        bookingValidation.getReturnedDateDis()
+        .should('be.visible')
+        .and('be.disabled')
+
+        bookingValidation.getNumberOfPassengersLabel()
+        .should('be.visible')
+
+        bookingValidation.getPassengerSelect()
+        .contains('1')
+
+        bookingValidation.getPassengerOneLabel()
+        .should('be.visible')
+
+        bookingValidation.getPassengerOneDropDown()
+        .contains('Adult (16-64)')
+
+        bookingValidation.getBookButton()
+        .should('be.visible')
+        .and('be.enabled')
+
+
+    })
+
+    it('Validate the Book your trip when round trip is selected', () =>{
+        bookingValidation.getRoundTripRadioButton().click()
+
+        bookingValidation.getOneWayRadioButton()
+        .should('not.be.checked')
+
+        bookingValidation.getCabinClassLabel()
+        .should('be.visible')
+
+        bookingValidation.getCabinClassDropdown()
+        .should('be.visible')
+
+        bookingValidation.getFromLabel()
+        .should('be.visible')
+        
+        bookingValidation.getFromDropdown()
+        .should('be.visible')
+
+        bookingValidation.getToLabel()
+        .should('be.visible')
+
+        bookingValidation.getToDropDown()
+        .should('be.visible')
+
+        bookingValidation.getDepartLabel()
+        .should('be.visible')
+
+        bookingValidation.getDepartDropDown()
+        .should('be.visible')
+
+        bookingValidation.getReturnLabel()
+        .should('be.visible')
+
+        bookingValidation.getReturnDateEna()
+        .should('be.visible') 
+
+        bookingValidation.getNumberOfPassengersLabel()
+        .should('be.visible')
+
+        bookingValidation.getPassengerSelect()
+        .contains('1')
+
+        bookingValidation.getPassengerOneLabel()
+        .should('be.visible')
+
+        bookingValidation.getPassengerOneDropDown()
+        .contains('Adult (16-64)')
+
+        bookingValidation.getBookButton()
+        .should('be.visible')
+        .and('be.enabled')
+    })
+
+    it('Validate booking for 1 passanger and one way', () =>{
+        bookingValidation.getOneWayRadioButton().click()
+
+        bookingValidation.getCabinClassDropdown().select('Business')
+
+        bookingValidation.getFromDropdown().select('Illinois')
+
+        bookingValidation.getToDropDown().find('select').select('Florida')
+
+        bookingValidation.getDepartDropDown().clear().type('05/23/2025')
+
+        bookingValidation.getPassengerSelect().select('1')
+
+        bookingValidation.getPassengerOneDropDown().find('select').select('Senior (65+)')
+
+        bookingValidation.getBookButton().click()
+
+        cy.get('div.mt-4').should('contain', 'Number of Passengers: 1');
+        cy.get('div.mt-4').should('contain', 'Passenger 1: Senior (65+)');
+        cy.get('div.mt-4').should('contain', 'Cabin class: Business');
+
+      
+
+    })
+
+    it('Validtae the booking for 1 passanger and round trip', () =>{
+
+     bookingValidation.getRoundTripRadioButton().click()
+
+     bookingValidation.getCabinClassDropdown().select('First')
+
+     bookingValidation.getFromDropdown().select('California')
+
+     bookingValidation.getToDropDown().find('select').select('Illinois')
+
+     bookingValidation.getDepartDropDown().clear().type('05/24/2025')
+
+     bookingValidation.getReturnDateEna().clear().type('06/24/2025')
+
+     bookingValidation.getPassengerSelect().select('1')
+
+     bookingValidation.getPassengerOneDropDown().find('select').select('Adult (16-64)')
+
+     bookingValidation.getBookButton().click()
+
+     cy.get('div[style*="width: 48%"]').first().within(() => {
+        cy.get('h1').should('have.text', 'DEPART')
+        cy.get('h3').should('have.text', 'CA to IL')
+        cy.get('p').should('have.text', 'Sat May 24 2025')
+      })
+
+      cy.get('div[style*="width: 48%"]').eq(1).within(() => {
+        cy.get('h1').should('have.text', 'RETURN')
+        cy.get('h3').should('have.text', 'IL to CA')
+        cy.get('p').should('have.text', 'Tue Jun 24 2025')
+      })
+
+      cy.get('div.mt-4').should('contain', 'Number of Passengers: 1')
+      cy.get('div.mt-4').should('contain', 'Passenger 1: Adult (16-64)')
+      cy.get('div.mt-4').should('contain', 'Cabin class: First')
+
+
+    })
+
+    it('Validate booking for 2 passangers one way', () =>{
+        bookingValidation.getOneWayRadioButton().click()
+
+        bookingValidation.getCabinClassDropdown().select('Premium Economy')
+
+        bookingValidation.getFromDropdown().select('New York')
+
+        bookingValidation.getToDropDown().find('select').select('Texas')
+
+        bookingValidation.getDepartDropDown().clear().type('05/18/2025')
+
+        bookingValidation.getPassengerSelect().select('2')
+
+        bookingValidation.getPassengerOneDropDown().find('select').select('Adult (16-64)')
+
+        bookingValidation.getPassengerTwoDropdown().select('Child (2-11)')
+
+        bookingValidation.getBookButton().click()
+
+        cy.get('div[style*="width: 48%"]').first().within(() => {
+            cy.get('h1').should('have.text', 'DEPART')
+            cy.get('h3').eq(0).should('have.text', 'NY to TX')         
+            cy.get('p').should('have.text', 'Sun May 18 2025')
+          })
+
+       cy.get('div.mt-4').should('contain', 'Number of Passengers: 2')
+       cy.get('div.mt-4').should('contain', 'Passenger 1: Adult (16-64)')
+       cy.get('div.mt-4').should('contain', 'Passenger 2: Child (2-11)')
+       cy.get('div.mt-4').should('contain', 'Cabin class: Premium Economy')
+
+
 
     })
 
 })
+
+
+
+
+
+
+
+
 
 
 
--- cypress/pages/BookingValidation.js
+++ cypress/pages/BookingValidation.js
@@ -1,6 +1,6 @@
 import { BasePage } from "./BasePage";
 
-class BookingValidation extends BasePage{
+export class BookingValidation extends BasePage{
     getOneWayRadioButton(){
         return cy.get('[class$="ml-0"] .mr-1')
     }
@@ -31,22 +31,56 @@ class BookingValidation extends BasePage{
             .find('select')
    }
 
+    getToLabel() {
+        return cy.get('.field:nth-of-type(4) > label')
+    }
+   
+    getToDropDown() {
+        return cy.get('.select').eq(2)
+    }
 
+    getDepartLabel() {
+        return cy.get('.field:nth-of-type(5) > label')
+    }
+    getDepartDropDown() {
+        return cy.get('input[type="text"][placeholder*="MM"]').first()
+    }
 
+    getReturnLabel() {
+        return cy.get('label[class="label"]').eq(5)
+    }
 
+    getReturnedDateDis() {
+        return cy.get('input[disabled]')
+    }
 
+    getReturnDateEna(){
+        return cy.get('input[placeholder="MM/DD/YY"]').eq(1)
+    }
+    
+    getNumberOfPassengersLabel() {
+        return cy.get('.field').eq(6).first()
+    }
 
+   
+    getPassengerSelect() {
+        return cy.get('select[style]').eq(3)
+    }
 
+    getPassengerOneDropDown() {
+        return cy.get('.select').eq(4)
+    }
 
+    getPassengerOneLabel() {
+        return cy.get("label[class='label']").eq(7)
+    }
 
-
-
-
-
-
-
-
-
+    getPassengerTwoDropdown(){
+        return cy.contains('label', 'Passenger 2')
+                .nextAll('div.select')
+                .first()
+                .find('select')
+       }
 
 
    getBookButton(){
